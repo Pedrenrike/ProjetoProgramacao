8e9db555c15e33f001921fdbf103084b1c292fd4
Gabriel Sousa <91401211+gabrielssousa@users.noreply.github.com>
2022-12-24 00:38:21 -0300

    Corrigindo erro

--- ProjetoEscola/src/Conexao.java
+++ ProjetoEscola/src/Conexao.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class Conexao {
     static Connection connection = null;
-    String url = "jdbc:mysql://localhost:3306/p";
+    String url = "jdbc:mysql://localhost:3306/projeto";
     String user = "root";
     String password = "";
 
